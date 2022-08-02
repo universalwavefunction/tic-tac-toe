7b9cfc1fba29c8b393f38a4256c3a506f4eaeda9
helan <helanmah@gmail.com>
2022-08-02 00:07:40 -0700

    add function

--- javascript.js
+++ javascript.js
@@ -1,30 +1,50 @@
-//start with 3x3 square grid gameboard
-//make button to add circle or x to square
-//attach move button to player and computer
-
-const gameBoard = {
-
-};
-
 let boardPieces = [];
 let board = document.querySelector('#gameboard');
 
-function newGame() {
+const gameBoard = (function() {
   for (let i = 0; i < 9; i++) {
     let boardSquare = document.createElement('div');
     board.appendChild(boardSquare).className = "boardSquare";
-    boardPieces.append(boardSquare);
-    boardSquare.addEventListener("click", () => {
-      if (!boardSquare.innerHTML) {
-        boardSquare.innerHTML = "X";
-      }
-    })
+    boardPieces.push(boardSquare);
   };
+})();
+
+const player1 = () => {
+  let moved = false;
+  const blah = () => {
+    const squares = document.querySelectorAll('.boardSquare')
+    squares.forEach(square => {
+      square.addEventListener("click", () => {
+      if (!square.innerHTML) {
+        square.innerHTML = "X"}})})
+  };
+
+  const listener = () => {
+    for (let i = 0; i < boardPieces.length; i++) {
+      const move = () => {
+        boardPieces[i].innerHTML = "X";
+        boardPieces.splice[i, 1];
+        moved = true;
+        console.log(boardPieces.length, !moved)
+        return moved;
+      };
+      if (!(boardPieces[i].innerHTML) && !moved) {
+        boardPieces[i].addEventListener("click", move)
+      }}}
+  return {listener}}
+
+const computer = {
+  move: function move() {
+    var randomnum = Math.floor(Math.random() * boardPieces.length)
+      if (!boardPieces[randomnum].innerHTML) {
+        boardPieces[randomnum].innerHTML = "O"}
+  }
 };
+
+const player = player1()
+player.listener()
+computer.move()
 
 //start game (pvp and pvc options), empties board, restarts
 //player 1 moves (event listener for click), square removed from array
 //then computer moves (selects random from array of unchosen squares)
-
-
-newGame();
