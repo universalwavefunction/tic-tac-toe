bb2e22c7536139a0caa95f686f0882690ad8f3cd
helan <helanmah@gmail.com>
2022-08-06 20:07:52 -0700

    adding multiple moves

--- javascript.js
+++ javascript.js
@@ -13,31 +13,53 @@ const player1 = () => {
   const squares = document.querySelectorAll('.boardSquare')
   let moved = false;
   const makemove = () => {
+    return new Promise(resolve => {
     squares.forEach(square => {
         square.addEventListener("click", () => {
         if (!square.innerHTML && !moved) {
           square.innerHTML = "X"
           moved = true
           boardPieces.splice(square, 1);
+          resolve()
           }});
-        })}
+        })})}
+  return {makemove, moved}};
 
-  return {makemove}};
-
-
-
-const computer = {
-  move: function move() {
+const computer = () => {
+  const move = () => {
     var randomnum = Math.floor(Math.random() * boardPieces.length)
       if (!boardPieces[randomnum].innerHTML) {
         boardPieces[randomnum].innerHTML = "O"}
+        boardPieces.splice(randomnum, 1)
   }
+  return {move}
 };
 
 const player = player1()
-player.makemove()
-computer.move()
+const computer1 = computer()
 
+async function newGame() {
+  let playermoved = true;
+  let computermoved = false;
+  moves = 0;
+  while (moves < 9) {
+    if (!computermoved) {
+      computer1.move();
+      computermoved = true
+      playermoved = false
+      moves++
+      console.log(moves,playermoved,computermoved)
+    }
+    if (!playermoved) {
+      await player.makemove();
+      playermoved = true
+      computermoved = false
+      moves++
+      console.log(moves,playermoved,computermoved)
+    }
+}};
+
+newGame()
 //start game (pvp and pvc options), empties board, restarts
-//player 1 moves (event listener for click), square removed from array
-//then computer moves (selects random from array of unchosen squares)
+//game playing functionality, player moves, then computer's turn,
+//then player's turn, etc until game is over
